54c1663f68c2286b10d87c6510750cb0c623ea31
M3fay1q <mrfayiq700@gmail.com>
2022-11-23 17:27:04 +0530

    ...

--- lib/src/view/screens/home/widgets/home_rounded_item_widget.dart
+++ lib/src/view/screens/home/widgets/home_rounded_item_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:myntra_clone_flutter/src/helper/app_routes.dart';
 import 'package:myntra_clone_flutter/src/view/screens/home/home_controller.dart';
 
 class HomeRoundedItemWidget extends StatelessWidget {
@@ -21,11 +22,16 @@ class HomeRoundedItemWidget extends StatelessWidget {
               padding: const EdgeInsets.all(7.0),
               child: Column(
                 children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.transparent,
-                    radius: Get.height * 0.046,
-                    backgroundImage: AssetImage(
-                        homeController.roundedMenuItems[index].image),
+                  InkWell(
+                    child: CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      radius: Get.height * 0.046,
+                      backgroundImage: AssetImage(
+                          homeController.roundedMenuItems[index].image),
+                    ),
+                    onTap: () {
+                      Get.toNamed(AppRoutes.brandMen);
+                    },
                   ),
                   Text(
                     homeController.roundedMenuItems[index].name,
